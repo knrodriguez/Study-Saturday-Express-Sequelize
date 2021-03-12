e19dff4f1698ff917ba3c021cbce4294df41d32e
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-03-12 09:59:06 -0500

    notes

--- db/models/student.js
+++ db/models/student.js
@@ -1,24 +1,26 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
+//allowNull and validate is found in Validations & Constraints
 const Student = db.define('student', {
     firstName: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false //constraint (checked by SQL)
     },
     lastName: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false //constraint (checked by SQL)
     },    
     email: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
+        allowNull: false, //constraint
+        validate: { //validation (checked by Sequelize before heading to the DB)
             isEmail: true
         }
     }
 });
 
+//found in Hooks -> Declaring Hooks (Method 3)
 Student.beforeCreate((student) => {
     let firstName = student.firstName;
     let lastName = student.lastName;
@@ -26,7 +28,7 @@ Student.beforeCreate((student) => {
     lastName = lastName.slice(0,1).toUpperCase() + lastName.slice(1);
     student.firstName = firstName;
     student.lastName = lastName;
-    
+
     //  DRYer method
     // let names = [student.firstName, student.lastName]
     //             .map(name => name.slice(0,1).toUpperCase() + name.slice(1));
